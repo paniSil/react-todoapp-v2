9a760b4aeea1aa34821f2b42d1a1b23a0a395280
paniSil <mzwonko@gmail.com>
2025-04-14 13:03:13 +0300

    Update mock-tasks.ts

--- src/data/mock-tasks.ts
+++ src/data/mock-tasks.ts
@@ -28,23 +28,5 @@ export const MOCK_TASKS = [
     id: '5',
     title: 'laboriosam mollitia et enim quasi adipisci quia provident illum',
     completed: false
-  },
-  {
-    userId: 1,
-    id: '6',
-    title: 'qui ullam ratione quibusdam voluptatem quia omnis',
-    completed: false
-  },
-  {
-    userId: 1,
-    id: '7',
-    title: 'illo expedita consequatur quia in',
-    completed: false
-  },
-  {
-    userId: 1,
-    id: '8',
-    title: 'quo adipisci enim quam ut ab',
-    completed: true
   }
 ]
